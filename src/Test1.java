3b5ded546985a6b6e5b6bb9bfc23c149c91eef46
ML Lab <ML Lab@MLLAB01>
2023-08-09 14:25:50 +0530

    Version-1

---
+++ src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int age=18;
+		
+		if(age>=18)
+		{
+			System.out.println("you are eligible for vote");
+		}
+		else
+		{
+			System.out.println("not eligible");
+		}
+
+	}
+
+}
